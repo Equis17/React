41eb2ad6bf8c0558f8641698793289d0d10c6b55
Equis <q1158582010@qq.com>
2019-06-13 17:00:12 +0800

    实现列表循环案例

---
+++ React实现电影项目/main.js
@@ -0,0 +1,9 @@
+import 'antd/dist/antd.css';
+import React from 'react'
+import ReactDom from 'react-dom'
+import App from './App.jsx'
+
+ReactDom.render(
+    <App></App>,
+    document.getElementById('app')
+);
--- webpack.config.js
+++ webpack.config.js
@@ -2,20 +2,20 @@ const path = require('path')
 const htmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-    entry: path.join(__dirname, './React路由/index.js'),
+    entry: path.join(__dirname, './React实现电影项目/main.js'),
     output: {
         path: path.join(__dirname, './dist'),
         filename: 'bundle.js'
     },
     plugins: [ // 插件
         new htmlWebpackPlugin({
-            template: path.join(__dirname, './React列表循环案例/index.html'),
+            template: path.join(__dirname, './React实现电影项目/index.html'),
             filename: 'index.html'
         })
     ],
     module: {
         rules: [
-            { test: /\.css$/, use: ['style-loader', 'css-loader?modules'] },
+            { test: /\.css$/, use: ['style-loader', 'css-loader'] },
             { test: /\.scss$/, use: ['style-loader', 'css-loader', 'sass-loader'] },
             { test: /\.(png|gif|bmp|jpg)$/, use: 'url-loader?limit=5000' },
             { test: /\.jsx?$/, use: 'babel-loader', exclude: /node_modules/ }
